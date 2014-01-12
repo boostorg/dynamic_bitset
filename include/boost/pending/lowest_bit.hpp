2e4b76b807a0c92b35f762d3a98cbe7ba9edb5e5
Alexey Stukalov <astukalov@cemm.oeaw.ac.at>
2014-01-12 19:46:13 +0100

    use __builtin_ctz() to speed-up lowest_bit()
    __builtin_ctz[l][l]() is built-in gcc/icc/clang operation to find
    the lowest bit, _BitScanForward[64]() is MSVC builtin one.
    It replaces the default arithmetic-based lowest_bit() implementation
    for the types that support this operation.
    
    The patch should improve the efficiency of dynamic_bitset<>
    traversing. See https://svn.boost.org/trac/boost/ticket/5158.
    
    Multiplatform support uses bits&pieces from
    boost/multiprecision/detail/bitscan.hpp.

--- include/boost/pending/lowest_bit.hpp
+++ include/boost/pending/lowest_bit.hpp
@@ -32,6 +32,58 @@ namespace boost {
 
     }
 
+#if defined(BOOST_MSVC) && (defined(_M_IX86) || defined(_M_X64))
+    template<>
+    BOOST_FORCEINLINE int lowest_bit(unsigned int x) {
+        assert(x >= 1); // PRE
+
+        unsigned long result;
+        _BitScanForward(&result, x);
+        return result;
+    }
+
+    template<>
+    BOOST_FORCEINLINE int lowest_bit(unsigned long x) {
+        assert(x >= 1); // PRE
+
+        unsigned long result;
+        _BitScanForward(&result, x);
+        return result;
+    }
+
+#ifdef _M_X64
+    template<>
+    BOOST_FORCEINLINE int lowest_bit(unsigned long long x) {
+        assert(x >= 1); // PRE
+
+        unsigned long result;
+        _BitScanForward64(&result, x);
+        return result;
+    }
+#endif
+
+#elif defined(BOOST_GCC) || defined(__clang__) || (defined(BOOST_INTEL) && defined(__GNUC__))
+    template<>
+    BOOST_FORCEINLINE int lowest_bit(unsigned int x) {
+        assert(x >= 1); // PRE
+
+        return __builtin_ctz( x );
+    }
+
+    template<>
+    BOOST_FORCEINLINE int lowest_bit(unsigned long x) {
+        assert(x >= 1); // PRE
+
+        return __builtin_ctzl( x );
+    }
+
+    template<>
+    BOOST_FORCEINLINE int lowest_bit(unsigned long long x) {
+        assert(x >= 1); // PRE
+
+        return __builtin_ctzll( x );
+    }
+#endif
 
 }
 
